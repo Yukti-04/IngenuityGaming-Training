f508550079a701348e5a629ea01e2c5725864420
Dipesh1jain <dipesh18jain@gmail.com>
2022-08-10 13:06:04 +0530

    files

---
+++ Day6/JavaScriptAssign.js
@@ -0,0 +1,67 @@
+// program1    Write a logic to perform the sum of n numbers entered by the user, stop if user enters negative number.
+
+// var sum=0;
+// var i=0;
+// while(i >=0){
+//         i=prompt("Enter Numbers to Add")
+//        if(i>=0){
+//         sum=sum+Number(i);
+//     }
+// }
+// alert("Sum of All Positive Number is: "+sum);
+
+
+
+//program2  2. Write a logic to create a game called Guess the Number. 
+    // Computer generate a Random Number.
+    // User will try to guess a number, if Guess is Correct, Print Msg "User Won" 
+    // User has three change to guess the number, if Fails, Print Msg "User Lose" 
+
+
+
+// let x = Math.floor((Math.random() * 10) + 1);
+// guessesleft=2
+// for (i=3;i>=1;i--)
+
+// {
+//     user=prompt("enter the number to guess between 0 to 10")
+// if(x==user)
+// {
+//     alert("Match Found" + x + "-" +user );
+//     alert("Hurray !! You Won ");
+
+// break;
+// }
+// else{
+//     alert("Match Not Found" +user)
+//     alert("Try Again!! Guesses Left " + guessesleft)
+
+// }
+// guessesleft--
+// }
+
+
+//program3  3.  Write code to create an Object containing information (Name, Age, Address(City, State, Pincode), Hobbies) and also having function to display all information. 
+
+
+
+
+// var student = {
+//     Name: "Dipesh",
+//     Age: 21,
+//     Address:{
+
+//         city: "jaipur",
+//         state: "rajasthan",
+//         pincode: 302021
+//     },
+//     hobbies: "Playing Games",
+
+//     details:function(){
+        
+
+// return this.Name + " " + this.Age + " " + this.Address +" "+ this.hobbies
+
+//     } 
+// }
+// console.log(student)
