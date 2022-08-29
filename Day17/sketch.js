ca7832ad73d990ebe4640a4449eb798ba5b46f86
Dipesh kala <dipesh18jain@gmail.com>
2022-08-29 03:34:40 -0700

    MarioGame

---
+++ Day17/sketch.js
@@ -0,0 +1,119 @@
+
+
+// Global Scope
+var bg, bgImage;
+var mario, mario_running;
+var ground;
+var brickImage, bricksGroup;
+var coins, coinsGroup;
+var coinScore = 0;
+
+// Load Assets
+function preload() {
+  bgImage = loadImage("images/bgnew.jpg");
+  mario_running = loadAnimation(
+    "images/mar1.png",
+    "images/mar2.png",
+    "images/mar3.png",
+    "images/mar4.png",
+    "images/mar5.png",
+    "images/mar6.png"
+  );
+  brickImage = loadImage("images/brick.png");
+  coinImage = loadAnimation("ImagesAssets/shuriken1.png","ImagesAssets/shuriken2.png","ImagesAssets/shuriken3.png","ImagesAssets/shuriken4.png")
+}
+
+// create basic Scaleton with their required credentials
+function setup() {
+  // Create Canvas
+  createCanvas(1000, 600);
+
+  // create Objects
+  bg = createSprite(600, 300, 150, 50);
+  mario = createSprite(200, 520, 150, 50);
+
+  // Add pictures on Objects
+  bg.addImage(bgImage);
+  mario.addAnimation("running", mario_running);
+
+  // Scale Objects
+  bg.scale = 0.5;
+  mario.scale = 0.2;
+
+  // create Ground
+  ground = createSprite(200, 580, 400, 10);
+  bricksGroup = new Group();
+  coinsGroup = new Group();
+}
+
+// Used to redraw the Objects on the canvas
+function draw() {
+  // Make background move and repeate
+  bg.velocityX = -5;
+  if (bg.x < 100) bg.x = bg.width / 4;
+
+  // mario Fly
+  if (keyDown("space")) mario.velocityY = -10;
+
+  // add Gravity
+  mario.velocityY = mario.velocityY + 0.5;
+
+  // mario stuck on ground
+  mario.collide(ground);
+  ground.visible = false;
+
+  // call GenerateBricks
+  generateBricks();
+
+  
+  for (var i = 0; i < bricksGroup.length; i++) {
+    var temp = bricksGroup.get(i);
+    if (mario.isTouching(temp)) {
+      mario.collide(temp);
+    }
+  }
+  if (mario.x < 200) {
+    mario.x = 200;
+  }
+  if (mario.y < 50) {
+    mario.y = 50;
+  }
+
+generateCoins();
+for (var i = 0; i < coinsGroup.length; i++) {
+    var temp = coinsGroup.get(i);
+    if (mario.isTouching(temp)) {
+      temp.destroy();
+      temp=null;
+      coinScore++;
+    }
+  }
+  // Redraw Objects
+  drawSprites();
+}
+function generateCoins() {
+    if (frameCount % 50 == 0) {
+      console.log(frameCount);
+  
+      var coin = createSprite(300, 100, 40, 10);
+      coin.y = random(200, 500);
+      coin.addAnimation("rotation",coinImage);
+      coin.scale = 0.5;
+      coin.velocityX = -5;
+      coin.lifetime = 400;
+      coinsGroup.add(coin);
+    }
+  }
+function generateBricks() {
+  if (frameCount % 80 == 0) {
+    console.log(frameCount);
+
+    var brick = createSprite(1200, 100, 40, 10);
+    brick.y = random(50, 450);
+    brick.addImage(brickImage);
+    brick.scale = 0.5;
+    brick.velocityX = -5;
+    brick.lifetime = 250;
+    bricksGroup.add(brick);
+  }
+}
